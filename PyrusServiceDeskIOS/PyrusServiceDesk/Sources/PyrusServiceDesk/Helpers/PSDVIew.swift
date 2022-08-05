9e4cbfd0200439c524c0a613dd204b34b9898c8c
Mikhail Rubanov <m.rubanov@dodopizza.com>
2022-08-05 16:25:13 +0500

    Move files to SPM

---
+++ PyrusServiceDeskIOS/PyrusServiceDesk/Sources/PyrusServiceDesk/Helpers/PSDVIew.swift
@@ -0,0 +1,18 @@
+import Foundation
+import UIKit
+
+class PSDView: UIView, Recolorable {
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        if #available(iOS 13.0, *) {
+            guard self.traitCollection.hasDifferentColorAppearance(comparedTo: previousTraitCollection) else {
+                return
+            }
+            recolor()
+        }
+    }
+    func recolor() {
+        ///Tis function must be overriten in subclass
+    }
+}
+
---
+++ PyrusServiceDeskIOS/PyrusServiceDesk/Sources/PyrusServiceDesk/PSDInfoView.swift
@@ -0,0 +1,14 @@
+import Foundation
+import UIKit
+
+///The showed
+public class PSDInfoView: UIView {
+    public override func removeFromSuperview() {
+        super.removeFromSuperview()
+        PSDMessagesStorage.pyrusUserDefaults()?.set(true, forKey: PSD_WAS_CLOSE_INFO_KEY)
+        if let vc  =  UIApplication.topViewController() as? PSDChatViewController{
+            vc.resizeTable()
+        }
+    }
+}
+
